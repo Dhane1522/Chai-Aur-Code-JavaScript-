ddeecac1abb12ac3414038098a13edae1c9a7b71
Roshan Aryal <119838051+Dhane1522@users.noreply.github.com>
2024-02-19 14:23:16 +0545

    ..

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,32 @@
+"use strict"// treats all JS code as newer version.
+
+//alert(3 + 3) not possible as we are using nodejs, not a browser.
+
+console.log(3
+    +
+    3) // Its a bad practice.// Code readability should be high.
+
+//Data Types..
+
+let name ="Roshan"
+let age = 18 
+let isloggedin = false
+let Hometown;
+
+
+// 1.Primitive DataTypes
+// number => Represents numeric values. It includes integers and floating-point numbers.
+// big int
+// string => Represents sequences of characters enclosed in single or double quotes.
+// boolean => Represents the logical entity and can have only two values; 'True' or 'False'.
+// null => Represents the intentional absence of any object value. Standalone value.
+// undefined => Represents the variables that has been declared but not asigned a value.
+// symbol => Represents a unique identifier.
+
+// 2.object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
---
+++ 01_Basics/03_ConversionOperstions.js
@@ -0,0 +1,34 @@
+// ******************* Conversion Of Primitive data types *****************
+
+// let score = null
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+// let IsLoggedIn = 33
+
+// let booleanIsloggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsloggedIn);
+// console.log(typeof booleanIsloggedIn)
+
+// 1 => true; 0 => false; (any positive or negative number is also true.)
+// "" => false
+// "Roshan" => true
+// null => false; undefined =>false
+
+let someNumber = 32
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+//converting primitive types to string often results in straightforward representation.
+
+
